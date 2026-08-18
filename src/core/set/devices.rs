3fbb41862885c11d47b48e80d1f421fe9f3432b9
Noah-Haefele <noah.haefele@gmx.de>
2026-08-18 17:35:54 +0200

    feat(set): add C++ bridge methods

--- src/core/set/devices.rs
+++ src/core/set/devices.rs
@@ -7,10 +7,11 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Device {
-    name: String,
+    #[serde(rename = "deviceName")]
+    device_name: String,
 
-    #[serde(rename = "shortName")]
-    short_name: String,
+    #[serde(rename = "deviceShortName")]
+    device_short_name: String,
 
     #[serde(rename = "deviceId")]
     device_id: i32,
@@ -74,20 +75,20 @@ impl DeviceManager {
             ConfigCache {
                 devices: vec![
                     Device {
-                        name: "1st Floor".to_string(),
-                        short_name: "1".to_string(),
+                        device_name: "1st Floor".to_string(),
+                        device_short_name: "1".to_string(),
                         device_id: 1,
                     },
 
                     Device {
-                        name: "2nd Floor".to_string(),
-                        short_name: "2".to_string(),
+                        device_name: "2nd Floor".to_string(),
+                        device_short_name: "2".to_string(),
                         device_id: 2,
                     },
 
                     Device {
-                        name: "3rd Floor".to_string(),
-                        short_name: "3".to_string(),
+                        device_name: "3rd Floor".to_string(),
+                        device_short_name: "3".to_string(),
                         device_id: 3,
                     },
                 ],
@@ -115,7 +116,7 @@ impl DeviceManager {
     }
 
     /// Loads both internal and config data and stores it in runtime struct cache
-    pub fn load(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+    fn load(&mut self) -> Result<(), Box<dyn std::error::Error>> {
         if self.config_path.is_file() {
             let config_json = read_from_disk(&self.config_path)?;
             self.config_cache = serde_json::from_str(&config_json)?;
@@ -126,6 +127,159 @@ impl DeviceManager {
         }
 
         Ok(())
+    }
+}
+
+#[cxx::bridge]
+mod ffi {
+    struct DeviceData {
+        device_name: String,
+        device_short_name: String,
+        device_id: i32,
+    }
+    
+    extern "Rust" {
+        type DeviceManager;
+        
+        fn get_all_devices(self: &DeviceManager) -> Vec<DeviceData>;
+        fn get_location_id(self: &DeviceManager) -> i32;
+        fn get_device_name(self: &DeviceManager, device_id: i32) -> String;
+        fn get_device_short_name(self: &DeviceManager, device_id: i32) -> String;
+        fn get_pref_call_id(self: &DeviceManager, num: i32) -> i32;
+        fn get_pref_model(self: &DeviceManager) -> Vec<DeviceData>;
+        fn get_pref_icon_path(self: &DeviceManager, num: i32) -> String;
+        fn set_location_id(self: &mut DeviceManager, device_id: i32);
+        fn set_pref_call_id(self: &mut DeviceManager, num: i32, device_id: i32);
+    }
+}
+
+impl DeviceManager {
+    fn get_all_devices(&self) -> Vec<ffi::DeviceData> {
+        self.config_cache
+            .devices
+            .iter()
+            .map(|device| ffi::DeviceData {
+                device_name: device.device_name.clone(),
+                device_short_name: device.device_short_name.clone(),
+                device_id: device.device_id,
+            })
+            .collect()
+    }
+
+    fn get_location_id(&self) -> i32 {
+        self.internal_settings_cache.location_id
+    }
+
+    fn get_device_config(&self, device_id: i32,) -> Option<&Device> {
+        self.config_cache
+            .devices
+            .iter()
+            .find(|device| device.device_id == device_id)
+    }
+
+    fn get_device_name(&self, device_id: i32) -> String {
+        self.get_device_config(device_id)
+            .map(|device| device.device_name.clone())
+            .unwrap_or_else(|| "Unknown...".to_string())
+    }
+
+    fn get_device_short_name(&self, device_id: i32) -> String {
+        if device_id == -1 {
+            return String::new();
+        }
+
+        self.get_device_config(device_id)
+            .map(|device| device.device_short_name.clone())
+            .unwrap_or_else(|| "?".to_string())
+    }
+
+    fn get_pref_call_id(&self, num: i32) -> i32 {
+        match num {
+            1 => self.internal_settings_cache.pref_call_id1,
+            2 => self.internal_settings_cache.pref_call_id2,
+            3 => self.internal_settings_cache.pref_call_id3,
+            _ => -1,
+        }
+    }
+
+    fn get_pref_model(&self) -> Vec<ffi::DeviceData> {
+        let location_id = self.get_location_id();
+
+        let mut model = vec![
+            ffi::DeviceData {
+                device_name: "Unassigned".to_string(),
+                device_short_name: String::new(),
+                device_id: -1,
+            }
+        ];
+
+        model.extend(
+            self.config_cache
+                .devices
+                .iter()
+                .filter(|device| device.device_id != location_id)
+                .map(|device| ffi::DeviceData {
+                    device_name: device.device_name.clone(),
+                    device_short_name: device.device_short_name.clone(),
+                    device_id: device.device_id,
+                })
+        );
+
+        model
+    }
+
+    fn get_pref_icon_path(&self, num: i32) -> String {
+        let pref_call_id = self.get_pref_call_id(num);
+
+        if pref_call_id == -1 {
+            String::new()
+        } else {
+            "qrc:/qt/qml/UiBridge/assets/icons/button/call.svg".to_string()
+        }
+    }
+
+    fn set_location_id(&mut self, device_id: i32) {
+        if self.get_location_id() != device_id {
+            self.internal_settings_cache.location_id = device_id;
+            
+            if self.internal_settings_cache.pref_call_id1 == device_id {
+                self.internal_settings_cache.pref_call_id1 = -1;
+            }
+
+            if self.internal_settings_cache.pref_call_id2 == device_id {
+                self.internal_settings_cache.pref_call_id2 = -1;
+            }
+
+            if self.internal_settings_cache.pref_call_id3 == device_id {
+                self.internal_settings_cache.pref_call_id3 = -1;
+            }
+        }
+    }
+
+    fn set_pref_call_id(&mut self, num: i32, device_id: i32) {
+        if self.get_pref_call_id(num) == device_id {
+            return
+        }
+
+        if device_id != -1 {
+            for i in [1, 2, 3] {
+                if i != num && self.get_pref_call_id(i) == device_id {
+                    match i {
+                        1 => self.internal_settings_cache.pref_call_id1 = -1,
+                        2 => self.internal_settings_cache.pref_call_id2 = -1,
+                        3 => self.internal_settings_cache.pref_call_id3 = -1,
+                        _ => {},
+                    }
+                }
+            }
+        }
+
+        match num {
+            1 => self.internal_settings_cache.pref_call_id1 = device_id,
+            2 => self.internal_settings_cache.pref_call_id2 = device_id,
+            3 => self.internal_settings_cache.pref_call_id3 = device_id,
+            _ => {},
+        }
     }
 }
 
