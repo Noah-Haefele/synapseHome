ce842705e1084fde82037221dca8b6e4b2453c7b
Noah-Haefele <noah.haefele@gmx.de>
2026-08-19 23:28:55 +0200

    feat(main): launch grpc server and start system api

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,37 @@
 mod core;
 
-use std::thread;
-use std::time::Duration;
+//use std::thread;
+//use std::time::Duration;
+use std::sync::Arc;
+use std::sync::Mutex;
+use tonic::transport::Server;
 
 use crate::core::set::devices::DeviceManager;
+use crate::core::api::grpc_server::ThisSystem;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let config_manager = DeviceManager::new()?;
+use crate::core::api::grpc_server::synapsed::settings::api::system_server::SystemServer;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> { 
+    let device_manager = DeviceManager::new()?;
+    let shared_device_manager = Arc::new(Mutex::new(device_manager));
+
+    let addr = "[::1]:50051".parse()?;
+    let grpc_server = ThisSystem::new(shared_device_manager);
+    let service = SystemServer::new(grpc_server);
+
+    Server::builder()
+        .add_service(service)
+        .serve(addr)
+        .await?;
+
+    Ok(())
 
     // Just for debuging now
-    loop {
-        thread::sleep(Duration::from_secs(5));
-        let _ = config_manager.save_config();
-        let _ = config_manager.save_internal_settings();
-        println!("Data saved");
-    }
+    //loop {
+    //    thread::sleep(Duration::from_secs(5));
+    //    let _ = config_manager.save_config();
+    //    let _ = config_manager.save_internal_settings();
+    //    println!("Data saved");
+    //}
 }
