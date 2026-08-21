468508adb7a84bbc81773143cd8f20248e052b34
Noah-Haefele <noah.haefele@gmx.de>
2026-08-21 12:43:55 +0200

    feat(main): bind display init in backend main entry point

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod core;
+mod platform;
 
 //use std::thread;
 //use std::time::Duration;
@@ -8,20 +9,29 @@ use tonic::transport::Server;
 
 use crate::core::state::devices::DeviceManager;
 use crate::core::api::grpc_server::ThisSystem;
+use crate::core::display::brightness::DisplayManager;
 
 use crate::core::api::grpc_server::synapsed::settings::api::system_server::SystemServer;
+use crate::core::api::grpc_server::synapsed::settings::api::display_server::DisplayServer;
+
+use crate::platform::linux::display_controller::DspCtrl;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> { 
-    let device_manager = DeviceManager::new()?;
-    let shared_device_manager = Arc::new(Mutex::new(device_manager));
+    let device_manager = Arc::new(Mutex::new(DeviceManager::new()?));
+    let display_controller = Arc::new(Mutex::new(DspCtrl::new()));
+    let display_manager = Arc::new(Mutex::new(DisplayManager::new(display_controller)?));
 
     let addr = "0.0.0.0:50051".parse()?;
-    let grpc_server = ThisSystem::new(shared_device_manager);
-    let service = SystemServer::new(grpc_server);
+
+    let grpc_server = ThisSystem::new(device_manager, display_manager);
+
+    let system_service = SystemServer::new(grpc_server.clone());
+    let display_service = DisplayServer::new(grpc_server);
 
     Server::builder()
-        .add_service(service)
+        .add_service(system_service)
+        .add_service(display_service)
         .serve(addr)
         .await?;
 
