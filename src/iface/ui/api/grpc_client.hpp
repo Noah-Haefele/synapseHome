24b27aac18b837452cdd685b1fd31a82a66beb0d
Noah-Haefele <noah.haefele@gmx.de>
2026-08-25 23:11:58 +0200

    feat(grpc-client): implement `pref_icon_paths` and `Ppef_short_names` and update client to refactored proto files

--- src/iface/ui/api/grpc_client.hpp
+++ src/iface/ui/api/grpc_client.hpp
@@ -1,17 +1,24 @@
 #pragma once
 
-#include <iostream>
-#include <memory>
-#include <vector>
 #include <grpcpp/grpcpp.h>
+#include <optional>
+#include <string>
 
 #include "settings_api.grpc.pb.h"
+#include "pref_api.grpc.pb.h"
 
-using synapsed::settings::api::System;
-using synapsed::settings::api::Display;
+// --- Settings Api ---
+using synapsed::api::settings::System;
+using synapsed::api::settings::Display;
+
+// --- Preference Api ---
+using synapsed::api::pref::PrefIconPaths;
+using synapsed::api::pref::PrefCallIds;
+using synapsed::api::pref::PrefShortNames;
+using synapsed::api::pref::PrefModels;
 
 // --- Data Message ---
-using ProtoDeviceData = synapsed::settings::api::DeviceData;
+using ProtoDeviceData = synapsed::api::helper::DeviceData;
 
 /**
  * @brief Client for communicating with the backend via gRPC.
@@ -25,12 +32,12 @@ public:
         const std::string& db);
 
     // --- System Settings ---
-    
+
     std::optional<std::vector<ProtoDeviceData>> get_all_devices();
     std::optional<int> get_location_id();
-    std::optional<int> get_pref_call_id1();
-    std::optional<int> get_pref_call_id2();
-    std::optional<int> get_pref_call_id3();
+    std::optional<int> get_pref1_call_id();
+    std::optional<int> get_pref2_call_id();
+    std::optional<int> get_pref3_call_id();
     std::optional<std::vector<ProtoDeviceData>> get_pref_model();
     void set_location_id(int device_id);
     void set_pref_call_id(int num, int device_id);
@@ -41,7 +48,19 @@ public:
     void set_brightness(int val);
     void set_display_time(int val);
 
+    // --- Control Grid ---
+    std::optional<std::string> get_pref1_icon_path();
+    std::optional<std::string> get_pref2_icon_path();
+    std::optional<std::string> get_pref3_icon_path();
+    std::optional<std::string> get_pref1_short_name();
+    std::optional<std::string> get_pref2_short_name();
+    std::optional<std::string> get_pref3_short_name();
+
 private:
     std::unique_ptr<System::Stub> system_stub_;
     std::unique_ptr<Display::Stub> display_stub_;
+    std::unique_ptr<PrefIconPaths::Stub> pref_icon_paths_stub_;
+    std::unique_ptr<PrefCallIds::Stub> pref_call_ids_stub_;
+    std::unique_ptr<PrefShortNames::Stub> pref_short_names_stub_;
+    std::unique_ptr<PrefModels::Stub> pref_models_stub_;
 };
