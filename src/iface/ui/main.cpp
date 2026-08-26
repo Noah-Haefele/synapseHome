43efd3b1f6eef384e3faa6255f72c2953aafb13f
Noah-Haefele <noah.haefele@gmx.de>
2026-08-26 15:26:12 +0200

    Bind new grpc signals client into frontend entry point - Exchange signals between `control_grid_bridge.hpp/cpp` and `grpc_signals_client.hpp/cpp` - Start grpc signals client in sperate thread due to `while` loop in client - Allow safe shutdown of the signalClient thread when the QCoreApplication is about to quit

--- src/iface/ui/main.cpp
+++ src/iface/ui/main.cpp
@@ -2,10 +2,12 @@
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 #include <grpcpp/grpcpp.h>
+#include <QThread>
 
 #include "grpc_client.hpp"
 #include "settings_bridge.hpp"
 #include "control_grid_bridge.hpp"
+#include "grpc_signals_client.hpp"
 
 
 int main(int argc, char *argv[])
@@ -19,6 +21,15 @@ int main(int argc, char *argv[])
     auto settingsBridge = new SettingsBridge(grpcClient, &app);
     auto controlGridBridge = new ControlGridBridge(grpcClient, &app);
 
+    auto signalClient = new SignalClient(channel);
+
+    QObject::connect(
+        signalClient,
+        &SignalClient::callIconChanged,
+        controlGridBridge,
+        &ControlGridBridge::iconChanged
+    );
+
     // c++ keeps memory contorl
     QQmlEngine::setObjectOwnership(settingsBridge, QQmlEngine::CppOwnership);
     QQmlEngine::setObjectOwnership(controlGridBridge, QQmlEngine::CppOwnership);
@@ -29,6 +40,33 @@ int main(int argc, char *argv[])
 
     QQmlApplicationEngine engine;
     engine.loadFromModule("UiBridge", "Main");
+
+    // --- Create thread for signal client ---
+
+    auto signalThread = new QThread(&app);
+
+    signalClient->moveToThread(signalThread);
+
+    QObject::connect(
+        signalThread,
+        &QThread::started,
+        signalClient,
+        &SignalClient::subscribe
+    );
+
+    signalThread->start();
+
+    // Safe thread stop
+    QObject::connect(
+        &app,
+        &QCoreApplication::aboutToQuit,
+        [&]() {
+            signalClient->stop();
+
+            signalThread->quit();
+            signalThread->wait();
+        }
+    );
 
     return app.exec();
 }
