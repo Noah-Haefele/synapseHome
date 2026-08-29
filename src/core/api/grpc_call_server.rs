208af885395d799f1fbc648ca5af2cea0a71a67a
Noah-Haefele <noah.haefele@gmx.de>
2026-08-29 23:01:06 +0200

    feat(api): implement call action api methods

--- src/core/api/grpc_call_server.rs
+++ src/core/api/grpc_call_server.rs
@@ -1,3 +1,4 @@
+use std::sync::Mutex;
 use tokio::sync::{broadcast, mpsc};
 use tokio_stream::wrappers::ReceiverStream;
 use tonic::{Request, Response, Status};
@@ -10,14 +11,31 @@ pub mod synapsed {
     }
 }
 
+use crate::core::act::call::call_handler::CallHandler;
+
+use synapsed::api::call::call_actions_server::CallActions;
 use synapsed::api::call::call_signals_server::CallSignals;
 
 use synapsed::api::call::Event;
 use synapsed::api::call::SubscribeRequest;
 
+use synapsed::api::call::InitiateRequest;
+
 #[derive(Debug, Clone)]
 pub struct LiveSignalsService {
     event_tx: broadcast::Sender<Event>,
+}
+
+pub struct CallApi {
+    call_handler: Mutex<CallHandler>,
+}
+
+impl CallApi {
+    pub fn new(call_handler: CallHandler) -> Self {
+        Self {
+            call_handler: Mutex::new(call_handler),
+        }
+    }
 }
 
 impl LiveSignalsService {
@@ -65,5 +83,34 @@ impl CallSignals for LiveSignalsService {
         });
 
         Ok(Response::new(ReceiverStream::new(rx)))
+    }
+}
+
+#[tonic::async_trait]
+impl CallActions for CallApi {
+    async fn initiate(&self, request: Request<InitiateRequest>) -> Result<Response<()>, Status> {
+        let req = request.into_inner();
+
+        let mut handler = self.call_handler.lock().unwrap();
+        handler.initiate_call(req.device_id);
+
+        println!("Initiate {}", req.device_id);
+        Ok(Response::new(()))
+    }
+
+    async fn accept(&self, _: Request<()>) -> Result<Response<()>, Status> {
+        let handler = self.call_handler.lock().unwrap();
+        handler.accept_call();
+
+        println!("Accept");
+        Ok(Response::new(()))
+    }
+
+    async fn end(&self, _: Request<()>) -> Result<Response<()>, Status> {
+        let mut handler = self.call_handler.lock().unwrap();
+        handler.end_call();
+
+        println!("End");
+        Ok(Response::new(()))
     }
 }
