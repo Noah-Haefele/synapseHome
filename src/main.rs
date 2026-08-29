c43c2a818c76deaf6341dffd09bed2b7be921aa5
Noah-Haefele <noah.haefele@gmx.de>
2026-08-29 23:03:01 +0200

    feat(main): build new call_actions_service and initialize call_handler

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,9 @@ mod platform;
 
 use std::sync::Arc;
 use std::sync::Mutex;
-use std::thread;
-use std::time::Duration;
 use tonic::transport::Server;
 
+use crate::core::api::grpc_call_server::CallApi;
 use crate::core::api::grpc_call_server::LiveSignalsService;
 use crate::core::api::grpc_server::CallIcons;
 use crate::core::api::grpc_server::ThisSystem;
@@ -25,10 +24,14 @@ use crate::core::api::grpc_server::synapsed::api::settings::system_server::Syste
 // --- Signals Api ---
 use crate::core::api::grpc_call_server::synapsed::api::call::call_signals_server::CallSignalsServer;
 
+// --- Call Api ---
+use crate::core::api::grpc_call_server::synapsed::api::call::call_actions_server::CallActionsServer;
+
 use crate::core::display::brightness::DisplayManager;
 
 use crate::core::state::devices::DeviceManager;
 
+use crate::core::act::call::call_handler::CallHandler;
 use crate::core::act::call::setup::CallSetup;
 
 use crate::platform::linux::display_controller::DspCtrl;
@@ -51,7 +54,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let grpc_server = ThisSystem::new(Arc::clone(&device_manager), display_manager);
     let grpc_server_call_icon = CallIcons::new(Arc::clone(&device_manager));
-    let grpc_call_server = LiveSignalsService::new();
+    let grpc_call_signals_server = LiveSignalsService::new();
+
+    let call_handler = CallHandler::new(grpc_call_signals_server.clone());
+
+    let grpc_call_actions_server = CallApi::new(call_handler);
 
     let system_service = SystemServer::new(grpc_server.clone());
     let display_service = DisplayServer::new(grpc_server.clone());
@@ -61,7 +68,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let pref_icon_paths = PrefIconPathsServer::new(grpc_server_call_icon.clone());
     let pref_short_names = PrefShortNamesServer::new(grpc_server_call_icon);
 
-    let call_signals_service = CallSignalsServer::new(grpc_call_server.clone());
+    let call_signals_service = CallSignalsServer::new(grpc_call_signals_server);
+    let call_actions_service = CallActionsServer::new(grpc_call_actions_server);
 
     Server::builder()
         .add_service(system_service)
@@ -71,6 +79,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .add_service(pref_icon_paths)
         .add_service(pref_short_names)
         .add_service(call_signals_service)
+        .add_service(call_actions_service)
         .serve(addr)
         .await?;
 
