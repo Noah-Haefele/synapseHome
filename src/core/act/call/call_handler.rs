565023335bff50ef0a39849a8e5f6545263593ef
Noah-Haefele <noah.haefele@gmx.de>
2026-08-30 22:29:48 +0200

    feat(act): add handlers for remote call actions

--- src/core/act/call/call_handler.rs
+++ src/core/act/call/call_handler.rs
@@ -7,9 +7,10 @@ pub struct CallHandler {
     grpc_call_signal_api: LiveSignalsService,
     mqtt_handler: Arc<Mutex<MqttHandler>>,
 
-    call_target_device_id: i32,
+    call_device_id: i32,
 }
 
+/// Methods that are called local meaning in this software by this device
 impl CallHandler {
     pub fn new(
         grpc_call_signal_api: LiveSignalsService,
@@ -19,7 +20,7 @@ impl CallHandler {
             grpc_call_signal_api,
             mqtt_handler,
 
-            call_target_device_id: -1,
+            call_device_id: -1,
         }
     }
 
@@ -30,7 +31,8 @@ impl CallHandler {
         location_id: i32,
         this_ip_address: &str,
     ) -> Result<(), Box<dyn std::error::Error>> {
-        self.call_target_device_id = target_device_id;
+        self.call_device_id = target_device_id;
+
         self.grpc_call_signal_api
             .trigger_call_state_changed("CALLING");
 
@@ -50,7 +52,32 @@ impl CallHandler {
     pub fn accept_call(&self) {}
 
     pub fn end_call(&mut self) {
-        self.call_target_device_id = -1;
+        self.call_device_id = -1;
+        self.grpc_call_signal_api.trigger_call_state_changed("IDLE");
+    }
+}
+
+/// Methods that are called when another device acts and sends a mqtt message
+impl CallHandler {
+    pub fn incoming_call(&mut self, source_device_id: i32, source_ip_address: &str) {
+        self.call_device_id = source_device_id;
+
+        self.grpc_call_signal_api
+            .trigger_call_state_changed("RINGING");
+    }
+
+    pub fn call_accepted(&self, source_device_id: i32, source_ip_address: &str) {
+        if self.call_device_id != source_device_id {
+            eprintln!("What a fish... This is an interesting exception :-()")
+        }
+        self.grpc_call_signal_api
+            .trigger_call_state_changed("CONNECTED");
+    }
+
+    pub fn call_ended(&self, source_device_id: i32, source_ip_address: &str) {
+        if self.call_device_id != source_device_id {
+            eprintln!("What a fish... This is an interesting exception :-()")
+        }
         self.grpc_call_signal_api.trigger_call_state_changed("IDLE");
     }
 }
