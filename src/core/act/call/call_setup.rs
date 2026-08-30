dbbe211e56a50ce57e2b6f4070aaa05bb9e43507
Noah-Haefele <noah.haefele@gmx.de>
2026-08-30 17:34:49 +0200

    refactor(act): remove DeviceManager dependency

--- src/core/act/call/call_setup.rs
+++ src/core/act/call/call_setup.rs
@@ -1,15 +1,14 @@
 use std::sync::{Arc, Mutex};
 
-use crate::core::state::devices::DeviceManager;
 use crate::networking::mqtt::mqtt_handler::MqttHandler;
 
 pub struct CallSetup {
-    mqtt_handler: MqttHandler,
+    mqtt_handler: Arc<Mutex<MqttHandler>>,
 }
 
 impl CallSetup {
     pub fn new(
-        mqtt_handler: MqttHandler,
+        mqtt_handler: Arc<Mutex<MqttHandler>>,
         location_id: i32,
     ) -> Result<Self, Box<dyn std::error::Error>> {
         let mut call_setup = Self { mqtt_handler };
@@ -25,14 +24,19 @@ impl CallSetup {
     ) -> Result<(), Box<dyn std::error::Error>> {
         // First unsubscribe old topics
         // This is e.g neccesairy when the device location id chages
-        for topic in self.mqtt_handler.get_subtopics() {
-            self.mqtt_handler.unsubscribe(topic)?;
+        let mut mqtt_handler = self
+            .mqtt_handler
+            .lock()
+            .map_err(|_| "Failed to lock MqttHandler")?;
+
+        for topic in mqtt_handler.get_subtopics() {
+            mqtt_handler.unsubscribe(topic)?;
         }
 
         // Make subtopic and subscribe
         let subtopic = format!("call/{}", location_id);
 
-        self.mqtt_handler.subscribe(subtopic)?;
+        mqtt_handler.subscribe(subtopic)?;
 
         Ok(())
     }
