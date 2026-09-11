64b40e9e6b45e772b2aca718773ab6029feed3e6
Noah-Haefele <noah.haefele@gmx.de>
2026-09-11 11:33:27 +0200

    chore(display): remove unused include Arc&Mutex

--- src/core/display/brightness.rs
+++ src/core/display/brightness.rs
@@ -3,8 +3,6 @@ use std::fs;
 use std::io::Write;
 use std::path::Path;
 use std::path::PathBuf;
-use std::sync::Arc;
-use std::sync::Mutex;
 use tempfile::NamedTempFile;
 
 use crate::platform::linux::display_controller::DspCtrl;
